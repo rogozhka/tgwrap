06c97fe4115d515447a7f8cbac5f0f74ee1c5545
rogozhka <serge.rogozhkin@gmail.com>
2018-01-13 14:56:16 +0300

    Add PhotoSize fields

--- api-photo-size.go
+++ api-photo-size.go
@@ -3,5 +3,27 @@ package tgwrap
 //
 // Telegram API object
 //
+// PhotoSize represents one size of a photo or a file / sticker thumbnail.
+//
 type PhotoSize struct {
+
+	//
+	// Unique identifier for this file
+	//
+	FileID string `json:"file_id"`
+
+	//
+	// Photo width
+	//
+	Width uint `json:"width"`
+
+	//
+	// Photo height
+	//
+	Height uint `json:"height"`
+
+	//
+	// Optional. File size
+	//
+	FileSize uint `json:"file_size,omitempty"`
 }
