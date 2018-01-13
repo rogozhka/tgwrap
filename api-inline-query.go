5b476c2800c02e3481d72fcd1a5ff1aec4b9be10
rogozhka <serge.rogozhkin@gmail.com>
2018-01-13 14:30:06 +0300

    Add InlineQuery fields

--- api-inline-query.go
+++ api-inline-query.go
@@ -3,5 +3,33 @@ package tgwrap
 //
 // Telegram API object
 //
+// This object represents an incoming inline query.
+// When the user sends an empty query, your bot could return some default or trending results.
+//
 type InlineQuery struct {
+
+	//
+	// Unique identifier for this query
+	//
+	ID string `json:"id"`
+
+	//
+	// Sender
+	//
+	From *User `json:"from"`
+
+	//
+	// Optional. Sender location, only for bots that request user location
+	//
+	Location *Location `json:"location,omitempty"`
+
+	//
+	// Text of the query (up to 512 characters)
+	//
+	Query string `json:"query"`
+
+	//
+	// Offset of the results to be returned, can be controlled by the bot
+	//
+	Offset string `json:"offset"`
 }
