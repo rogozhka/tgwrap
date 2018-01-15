8cf9baed8450e0dc4342e156edbd016c5ac29b2a
rogozhka <serge.rogozhkin@gmail.com>
2018-01-16 01:24:55 +0300

    Add Video fields

--- api-video.go
+++ api-video.go
@@ -3,5 +3,42 @@ package tgwrap
 //
 // Telegram API object
 //
+// Video represents a video file.
+//
 type Video struct {
+
+	//
+	// Unique identifier for this file
+	//
+	ID string `json:"file_id"`
+
+	//
+	// Video width as defined by sender
+	//
+	Width uint `json:"width"`
+
+	//
+	// Video height as defined by sender
+	//
+	Height uint `json:"height"`
+
+	//
+	// Duration of the video in seconds as defined by sender
+	//
+	Duration uint `json:"duration"`
+
+	//
+	// Optional. Video thumbnail
+	//
+	Thumb *PhotoSize `json:"thumb,omitempty"`
+
+	//
+	// Optional. Mime type of a file as defined by sender
+	//
+	MIMEType string `json:"mime_type,omitempty"`
+
+	//
+	// Optional. File size
+	//
+	FileSize uint `json:"file_size,omitempty"`
 }
