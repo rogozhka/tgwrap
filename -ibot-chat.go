99ad7d74d3b52e4d5c358b19b5762be94a6063bd
rogozhka <serge.rogozhkin@gmail.com>
2018-01-17 23:33:01 +0300

    Update comments: IBotChat & SendChatAction

--- -ibot-chat.go
+++ -ibot-chat.go
@@ -1,6 +1,11 @@
 package tgwrap
 
+//
+// IBotChat is a group of methods for chat manipulation
+// used as a part of common IBot API interface
+//
 type IBotChat interface {
+
 	//
 	// Use SendChatAction method when you need to tell the user
 	// that something is happening on the bot's side. The status
@@ -8,10 +13,10 @@ type IBotChat interface {
 	// your bot, Telegram clients clear its typing status).
 	// Returns True on success.
 	//
-	// @param chatID Unique identifier for the target chat
+	// chatID: unique identifier for the target chat
 	// or username of the target channel (in the format @channelusername)
 	//
-	// @param action Type of action to broadcast.
+	// action: type of action to broadcast.
 	//
 	SendChatAction(chatID interface{}, action ChatActions) (bool, error)
 }
