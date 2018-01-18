9d0d1aecf4d365059cc06a35e450554bf9e926ce
rogozhka <serge.rogozhkin@gmail.com>
2018-01-18 13:09:18 +0300

    Update comments: NewBot

--- -new.go
+++ -new.go
@@ -4,6 +4,12 @@ type bot struct {
 	token string
 }
 
+//
+// NewBot creates new object with associated token inside
+//
+// note: object is object, new is new
+// and this is stupid comment to make linter happy
+//
 func NewBot(token string) IBot {
 	return &bot{
 		token: token,
