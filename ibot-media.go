f11f6d4c265e9c4b1d5503d698bff60872a13f67
Sergey Donskoy <serj.donskoj.2012@gmail.com>
2018-02-03 15:20:54 +0200

    Implemented sendVenue() endpoint: - Added method skeleton - Added method implementation - Added test

--- ibot-media.go
+++ ibot-media.go
@@ -122,4 +122,22 @@ type IBotMedia interface {
 	// opt: (can be nil) optional params
 	//
 	SendLocation(chatID interface{}, latitude float64, longitude float64, opt *SendLocationOpt) (*Message, error)
+
+	// SendVenue is used to send information about a venue.
+	// On success, the sent Message is returned.
+	//
+	// chatID: (uint64 or string) Unique identifier for the target chat
+	// or username of the target channel (in the format @channelusername)
+	//
+	// latitude: (float64) Latitude of the venue.
+	//
+	// longitude: (float64) Longitude of the venue.
+	//
+	// title: (string) Name of the venue.
+	//
+	// address: (string) Address of the venue.
+	//
+	// opt: (can be nil) optional params
+	//
+	SendVenue(chatID interface{}, latitude float64, longitude float64, title string, address string, opt *SendVenueOpt) (*Message, error)
 }
