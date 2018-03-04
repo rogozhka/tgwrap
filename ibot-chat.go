048f6745ce8e8535832e6d7633c6f5615a04ef9e
Sergey <serj.donskoj.2012@gmail.com>
2018-03-04 17:13:24 +0200

    Implemented restrictChatMethod() method FLSM - created method skeleton - implemented method - created tests - updated Chat interface

--- ibot-chat.go
+++ ibot-chat.go
@@ -19,4 +19,17 @@ type IBotChat interface {
 	// action: type of action to broadcast.
 	//
 	SendChatAction(chatID interface{}, action ChatActions) (bool, error)
+
+	// RestrictChatMember is used to restrict a user in a supergroup.
+	// The bot must be an administrator in the supergroup for this to work and must have the appropriate admin rights.
+	// Pass True for all boolean parameters to lift restrictions from a user. Returns True on success.
+	//
+	// chatID: (uint64 or string) Unique identifier for the target chat
+	// or username of the target channel (in the format @channelusername)
+	//
+	// userID: (int64) Unique identifier of the target user
+	//
+	// opt: (can be nil) optional params
+	//
+	RestrictChatMember(chatID interface{}, userID uint64, opt *RestrictChatMemberOpt) (bool, error)
 }
---
+++ wrap-restrict-chat-member.go
@@ -0,0 +1,83 @@
+package tgwrap
+
+import (
+	"fmt"
+)
+
+//
+// RestrictChatMemberOpt represents optional params for RestrictChatMemberOpt()
+//
+type RestrictChatMemberOpt struct {
+
+	//
+	// Date when the user will be unbanned, unix time.
+	// If user is banned for more than 366 days or less than 30 seconds from the current time they are considered to be banned forever
+	//
+	UntilDate int64
+
+	//
+	// Pass True, if the user can send text messages, contacts, locations and venues
+	//
+	CanSendMessages bool
+
+	//
+	// Pass True, if the user can send audios, documents, photos, videos, video notes and voice notes, implies can_send_messages
+	//
+	CanSendMediaMessages bool
+
+	//
+	// Pass True, if the user can send animations, games, stickers and use inline bots, implies can_send_media_messages
+	//
+	CanSendOtherMessages bool
+
+	//
+	// Pass True, if the user may add web page previews to their messages, implies can_send_media_messages
+	//
+	CanAddWebPagePreviews bool
+}
+
+// RestrictChatMember is used to restrict a user in a supergroup.
+// The bot must be an administrator in the supergroup for this to work and must have the appropriate admin rights.
+// Pass True for all boolean parameters to lift restrictions from a user. Returns True on success.
+//
+// chatID: (uint64 or string) Unique identifier for the target chat
+// or username of the target channel (in the format @channelusername)
+//
+// userID: (int64) Unique identifier of the target user
+//
+// opt: (can be nil) optional params
+//
+func (p *bot) RestrictChatMember(chatID interface{}, userID uint64, opt *RestrictChatMemberOpt) (bool, error) {
+
+	type sendFormat struct {
+		ChatID string `json:"chat_id"`
+
+		UserID uint64 `json:"user_id"`
+
+		RestrictChatMemberOpt `json:",omitempty"`
+	}
+
+	dataSend := sendFormat{
+		ChatID: fmt.Sprint(chatID),
+		UserID: userID,
+	}
+
+	if opt != nil {
+		dataSend.RestrictChatMemberOpt = *opt
+	}
+
+	var resp struct {
+		GenericResponse
+
+		Result bool `json:"result"`
+	}
+
+	var sender fCommandSender = p.sendJSON
+
+	err := p.getAPIResponse("restrictChatMember", sender, dataSend, &resp)
+	if err != nil {
+		return false, fmt.Errorf("getAPIResponse ERROR:%v", err)
+	}
+
+	return resp.Result, nil
+}
