dc6a229175249403851d1e468f2822e78b3ad7a9
rogozhka <serge.rogozhkin@gmail.com>
2018-03-11 16:50:16 +0300

    Fix UnbanChatMember test draft

--- ibot-chat.go
+++ ibot-chat.go
@@ -19,16 +19,4 @@ type IBotChat interface {
 	// action: type of action to broadcast.
 	//
 	SendChatAction(chatID interface{}, action ChatActions) (bool, error)
-
-	// UnbanChatMember is used to unban a previously kicked user in a supergroup or channel.
-	// The user will not return to the group or channel automatically, but will be able to join via link, etc.
-	// The bot must be an administrator for this to work.
-	// Returns True on success.
-	//
-	// chatID: (uint64 or string) Unique identifier for the target chat
-	// or username of the target channel (in the format @channelusername)
-	//
-	// userID: (int64) Unique identifier of the target user
-	//
-	UnbanChatMember(chatID interface{}, userID uint64) (bool, error)
 }
--- wrap-unban-chat-member.go
+++ wrap-unban-chat-member.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 )
 
+//
 // UnbanChatMemberis used to unban a previously kicked user in a supergroup or channel.
 // The user will not return to the group or channel automatically, but will be able to join via link, etc.
 // The bot must be an administrator for this to work.
