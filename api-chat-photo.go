ac3e3b5f92522836e8c28e83de2412f82cceaa92
rogozhka <serge.rogozhkin@gmail.com>
2020-03-11 21:53:35 +0300

    ChatPhoto: add bigFileIDs; comments

--- api-chat-photo.go
+++ api-chat-photo.go
@@ -1,19 +1,23 @@
 package tgwrap
 
-//
-// ChatPhoto is a Telegram API object
-//
+// ChatPhoto is a Telegram API object.
 type ChatPhoto struct {
 
-	//
-	// Unique file identifier of small (160x160) chat photo.
+	// SmallFileID - unique file identifier of small (160x160) chat photo.
 	// This file_id can be used only for photo download.
-	//
 	SmallFileID string `json:"small_file_id"`
 
-	//
-	// Unique file identifier of big (640x640) chat photo.
+	// SmallFileUniqueID - unique file identifier of small (160x160) chat photo,
+	// which is supposed to be the same over time and for different bots.
+	// Can't be used to download or reuse the file.
+	SmallFileUniqueID string `json:"small_file_unique_id"`
+
+	// BigFileID - unique file identifier of big (640x640) chat photo.
 	// This file_id can be used only for photo download.
-	//
 	BigFileID string `json:"big_file_id"`
+
+	// BigFileUniqueID - unique file identifier of big (640x640) chat photo,
+	// which is supposed to be the same over time and for different bots.
+	// Can't be used to download or reuse the file.
+	BigFileUniqueID string `json:"big_file_unique_id"`
 }
