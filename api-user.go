08aaefc8cac55f286c90ff600862477e3254ca43
rogozhka <serge.rogozhkin@gmail.com>
2020-03-11 21:50:05 +0300

    User: add fields; comments

--- api-user.go
+++ api-user.go
@@ -1,36 +1,32 @@
 package tgwrap
 
-//
 // User represents a Telegram user or bot.
-//
 type User struct {
-	//
-	// User‘s or bot’s id
-	//
-	ID uint64 `json:"id"`
 
-	//
-	// True for bot
-	//
+	// User‘s or bot’s id
+	ID int64 `json:"id"`
+
+	// IsBot - true for bot.
 	IsBot bool `json:"is_bot"`
 
-	//
-	// User‘s or bot’s first name
-	//
+	// FirstName. User‘s or bot’s first name.
 	FirstName string `json:"first_name"`
 
-	//
-	// Optional. User‘s or bot’s last name
-	//
+	// LastName. Optional. User‘s or bot’s last name
 	LastName string `json:"last_name"`
 
-	//
-	// Optional. User‘s or bot’s username
-	//
-	UserName string `json:"username"`
+	// UserName. Optional. User‘s or bot’s username
+	UserName string `json:"username,omitempty"`
 
-	//
-	// Optional. IETF language tag of the user's language
-	//
-	LanguageCode string `json:"language_code"`
+	// LanguageCode. Optional. IETF language tag of the user's language
+	LanguageCode string `json:"language_code,omitempty"`
+
+	// CanJoinGroups. True, if the bot can be invited to groups. Returned only in getMe.
+	CanJoinGroups bool `json:"can_join_groups,omitempty"`
+
+	// CanReadAllGroupMessages. True, if privacy mode is disabled for the bot. Returned only in getMe.
+	CanReadAllGroupMessages bool `json:"can_read_all_group_messages,omitempty"`
+
+	// SupportsInlineQueries. Optional. True, if the bot supports inline queries. Returned only in getMe.
+	SupportsInlineQueries bool `json:"supports_inline_queries,omitempty"`
 }
