48d4cc7c5aa0d6a67515acd387acf93d149b891b
rogozhka <serge.rogozhkin@gmail.com>
2020-03-11 21:43:18 +0300

    Audio: add fields, edit comments.

--- api-audio.go
+++ api-audio.go
@@ -1,37 +1,31 @@
 package tgwrap
 
-//
 // Audio represents an audio file to be treated as music by the Telegram clients.
-//
 type Audio struct {
 
-	//
-	// Unique identifier for this file
-	//
+	// ID is Identifier for this file, which can be used to download or reuse the file.
 	ID string `json:"file_id"`
 
-	//
-	// Duration of the audio in seconds as defined by sender
-	//
-	Duration uint `json:"duration"`
+	// FileUniqueID - unique identifier for this file, which is supposed
+	// to be the same over time and for different bots.
+	// Can't be used to download or reuse the file.
+	FileUniqueID string `json:"file_unique_id"`
 
-	//
-	// Optional. Performer of the audio as defined by sender or by audio tags
-	//
+	// Duration of the audio in seconds as defined by sender.
+	Duration int `json:"duration"`
+
+	// Performer. Optional. Performer of the audio as defined by sender or by audio tags.
 	Performer string `json:"performer,omitempty"`
 
-	//
-	// Optional. Title of the audio as defined by sender or by audio tags
-	//
+	// Title. Optional. Title of the audio as defined by sender or by audio tags
 	Title string `json:"title,omitempty"`
 
-	//
-	// Optional. MIME type of the file as defined by sender
-	//
+	// MIMEType. Optional. MIME type of the file as defined by sender.
 	MIMEType string `json:"mime_type,omitempty"`
 
-	//
-	// Optional. File size
-	//
-	TitleSize uint `json:"file_size,omitempty"`
+	// FileSize. Optional. File size bytes.
+	FileSize int `json:"file_size,omitempty"`
+
+	// Thumb. Optional. Thumbnail of the album cover to which the music file belongs.
+	Thumb *PhotoSize `json:"thumb,omitempty"`
 }
