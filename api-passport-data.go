d89d25d0d491560832cd28c400768cd7f6348d6b
rogozhka <serge.rogozhkin@gmail.com>
2020-03-11 21:52:03 +0300

    Add PassportData draft

---
+++ api-passport-data.go
@@ -0,0 +1,8 @@
+package tgwrap
+
+//Contains information about Telegram Passport data shared with the bot by the user.
+type PassportData struct {
+	//
+	//data	Array of EncryptedPassportElement	Array with information about documents and other Telegram Passport elements that was shared with the bot
+	//credentials	EncryptedCredentials	Encrypted credentials required to decrypt the data
+}
