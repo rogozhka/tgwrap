0681db5c273ef7500ae27af624956042c337adc4
rogozhka <serge.rogozhkin@gmail.com>
2020-03-11 21:49:47 +0300

    Venue: add FoursquareType; comments

--- api-venue.go
+++ api-venue.go
@@ -1,27 +1,21 @@
 package tgwrap
 
-//
-// Venue represents a place, meeting point, event point
-//
+// Venue represents a place, meeting point, event point.
 type Venue struct {
 
-	//
-	// Venue location
-	//
+	// Location is Venue location.
 	Location *Location `json:"location"`
 
-	//
-	// Name of the venue
-	//
+	// Title is Name of the venue.
 	Title string `json:"title"`
 
-	//
-	// Address of the venue
-	//
+	// Address of the venue.
 	Address string `json:"address"`
 
-	//
-	// Optional. Foursquare identifier of the venue
-	//
+	// FoursquareID. Optional. Foursquare identifier of the venue.
 	FoursquareID string `json:"foursquare_id,omitempty"`
+
+	// FoursquareType. Optional. Foursquare type of the venue.
+	// (For example, “arts_entertainment/default”, “arts_entertainment/aquarium” or “food/icecream”.)
+	FoursquareType string `json:"foursquare_type"`
 }
