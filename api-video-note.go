f32461dedea1db0c2124403a8ce5d5de96cd61b5
rogozhka <serge.rogozhkin@gmail.com>
2020-03-11 21:47:46 +0300

    Get rid of uint fields in favour to int

--- api-video-note.go
+++ api-video-note.go
@@ -1,32 +1,20 @@
 package tgwrap
 
-//
 // VideoNote represents a video message (available in Telegram apps as of v.4.0).
-//
 type VideoNote struct {
 
-	//
-	// Unique identifier for this file
-	//
+	// ID is unique identifier for this file.
 	ID string `json:"file_id"`
 
-	//
-	// Video width and height as defined by sender
-	//
-	Length uint `json:"length"`
+	// Length. Video width and height as defined by sender
+	Length int `json:"length"`
 
-	//
-	// Duration of the video in seconds as defined by sender
-	//
-	Duration uint `json:"duration"`
+	// Duration of the video in seconds as defined by sender.
+	Duration int `json:"duration"`
 
-	//
-	// Optional. Video thumbnail
-	//
+	// Thumb. Optional. Video thumbnail.
 	Thumb *PhotoSize `json:"thumb,omitempty"`
 
-	//
-	// Optional. File size
-	//
-	FileSize uint `json:"file_size,omitempty"`
+	// FileSize. Optional. File size bytes.
+	FileSize int `json:"file_size,omitempty"`
 }
