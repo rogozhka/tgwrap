b2c4037b6e0c5facb7033b6d727213e0815e33bd
rogozhka <serge.rogozhkin@gmail.com>
2020-03-26 23:39:36 +0300

    Add getFile

---
+++ wrap-get-file_test.go
@@ -0,0 +1,21 @@
+package tgwrap
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBot_GetFile(t *testing.T) {
+
+	bot := createTestBotFromEnv()
+
+	fileID := testEnvStringValue("TGWRAP_TEST_FILE_ID")
+
+	file, err := bot.GetFile(fileID)
+	assert.Nil(t, err)
+	assert.NotNil(t, file)
+	assert.True(t, len(file.FilePath) > 0)
+	assert.True(t, file.FileSize > 0)
+	assert.Equal(t, fileID, file.FileID)
+}
